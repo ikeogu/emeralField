f2a818b2c4e5c97c9a95b8b3915a6fba45d6d1f4
ikeogu <ikeogu322@gmail.com>
2020-10-21 10:36:32 +0100

    uN ASSIGNED SUBJECT LINK

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -39,179 +39,168 @@
 </head>
 
 <body id="page-top">
+    
+  <div id="wrapper">
     <div id="app">
-        <div id="wrapper">
-
-            <!-- Sidebar -->
-            <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+      <!-- Sidebar -->
+      <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+  
+        <!-- Sidebar - Brand -->
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+          <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+          </div>
+          <div class="sidebar-brand-text mx-3">EmeraldField <sup>Portal</sup></div>
+        </a>
+  
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0">
+  
+        <!-- Nav Item - Dashboard -->
+        <li class="nav-item active">
+          <a class="nav-link" href="/">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+        </li>
+  
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+  
         
-              <!-- Sidebar - Brand -->
-              <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-icon rotate-n-15">
-                  <i class="fas fa-laugh-wink"></i>
-                </div>
-                <div class="sidebar-brand-text mx-3">EmeraldField <sup>Portal</sup></div>
-              </a>
         
-              <!-- Divider -->
-              <hr class="sidebar-divider my-0">
-        
-              <!-- Nav Item - Dashboard -->
-              <li class="nav-item active">
-                <a class="nav-link" href="index.html">
-                  <i class="fas fa-fw fa-tachometer-alt"></i>
-                  <span>Dashboard</span></a>
-              </li>
-        
-              <!-- Divider -->
-              <hr class="sidebar-divider">
-        
-             
-             
-              <!-- Divider -->
-              <hr class="sidebar-divider">
-        
-              <!-- Heading -->
-              <div class="sidebar-heading">
-                
-              </div>
-        
-              <!-- Nav Item - Pages Collapse Menu -->
-              <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-                  <i class="fas fa-fw fa-folder"></i>
-                  <span>Students</span>
-                </a>
-                <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                  <div class="bg-white py-2 collapse-inner rounded">
-                    
-                      <a class="collapse-item" href="{{route('students.index')}}">
-                          <i class="fas fa-fw fa-chart-area"></i>
-                          <span> All Students</span></a>
-                      
-                      <a class="collapse-item" href="{{route('hschool')}}">High School Students</a>
-                    <a class="collapse-item" href="{{route('yschool')}}">Year School Students</a>
-                    <a class="collapse-item" href="{{route('eschool')}}">Early Year</a>
-                    <div class="collapse-divider"></div>
-                    
-                  </div>
-                </div>
-              </li>
-        
-              <!-- Nav Item - Charts -->
-              <li class="nav-item">
-                <a class="nav-link" href="{{route('term')}}">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Create new Term</span></a>
-                </li>
-              
-              <li class="nav-item">
-              <a class="nav-link" href="{{route('class.index')}}">
-                  <i class="fas fa-fw fa-house"></i>
-                  <span>Classes</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{route('subjects.index')}}">
-                    <i class="fas fa-fw fa-book"></i>
-                    <span>Subjects</span></a>
-                </li>
-                
-              <!-- Nav Item - Tables -->
-              <li class="nav-item">
-              <a class="nav-link" href="{{route('teachers.index')}}">
-                  <i class="fas fa-fw fa-user"></i>
-                  <span>Teacher</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{route('grades.index')}}">
-                    <i class="fas fa-fw fa-setting-o"></i>
-                    <span>Grade Settings</span></a>
-                </li>
-        
-              <!-- Divider -->
-              <hr class="sidebar-divider d-none d-md-block">
-        
-              <!-- Sidebar Toggler (Sidebar) -->
-              <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-              </div>
-              
-            </ul>
-            <!-- End of Sidebar -->
-        
-            <!-- Content Wrapper -->
-            <div id="content-wrapper" class="d-flex flex-column">
-        
-              <!-- Main Content -->
-                <div id="content">
-        
-                    <!-- Topbar -->
-                    <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-                
-                        <!-- Sidebar Toggle (Topbar) -->
-                         <!-- Sidebar Toggle (Topbar) -->
-                        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                          <i class="fa fa-bars"></i>
-                        </button>
-
-                        
-                        <button onclick="goBack()" class=" btn btn-warning text-white btn-block btn-sm">Go Back</button>
-
-
-                
-
-                        <!-- Topbar Navbar -->
-                        <ul class="navbar-nav ml-auto">
-                
-                           
-                
-                           
-                
-                            
-                
-                            <div class="topbar-divider d-none d-sm-block"></div>
-                
-                            <!-- Nav Item - User Information -->
-                            <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            {{-- <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{Auth::user()->name}}</span> --}}
-                                <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
-                            </a>
-                            <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                                
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Logout
-                                </a>
-                            </div>
-                            </li>
-                
-                        </ul>
-                
-                    </nav>
-                    <main>
-                        @yield('content')
-                    </main>
-                </div>
-                <!-- End of Main Content -->
-            
-                <!-- Footer -->
-                <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                    <span>Copyright &copy; Your Website 2019</span>
-                    </div>
-                </div>
-                </footer>
-                <!-- End of Footer -->
-      
-            </div>
-            <!-- End of Content Wrapper -->
-      
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+  
+        <!-- Heading -->
+        <div class="sidebar-heading">
+          
         </div>
-        <!-- End of Page Wrapper -->
+  
+        <!-- Nav Item - Pages Collapse Menu -->
+        <li class="nav-item">
+          <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>Students</span>
+          </a>
+          <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+              
+                <a class="collapse-item" href="{{route('students.index')}}">
+                    <i class="fas fa-fw fa-chart-area"></i>
+                    <span> All Students</span></a>
+                
+                <a class="collapse-item" href="{{route('hschool')}}">High School Students</a>
+              <a class="collapse-item" href="{{route('yschool')}}">Year School Students</a>
+              <a class="collapse-item" href="{{route('eschool')}}">Early Year</a>
+              <div class="collapse-divider"></div>
+              
+            </div>
+          </div>
+        </li>
+  
+        <!-- Nav Item - Charts -->
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('term')}}">
+              <i class="fas fa-fw fa-chart-area"></i>
+              <span>Create new Term</span></a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('class.index')}}">
+            <i class="fas fa-fw fa-house"></i>
+            <span>Classes</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('subjects.index')}}">
+              <i class="fas fa-fw fa-book"></i>
+              <span>Subjects</span></a>
+          </li>
+          
+        <!-- Nav Item - Tables -->
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('teachers.index')}}">
+            <i class="fas fa-fw fa-user"></i>
+            <span>Teacher</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('grades.index')}}">
+              <i class="fas fa-fw fa-setting-o"></i>
+              <span>Grade Settings</span></a>
+        </li>
+  
+        <!-- Divider -->
+        <hr class="sidebar-divider d-none d-md-block">
+  
+        <!-- Sidebar Toggler (Sidebar) -->
+        <div class="text-center d-none d-md-inline">
+          <button class="rounded-circle border-0" id="sidebarToggle"></button>
+        </div>
+        
+      </ul>
+      <!-- End of Sidebar -->
+  
+      <!-- Content Wrapper -->
+      <div id="content-wrapper" class="d-flex flex-column">
+  
+        <!-- Main Content -->
+          <div id="content">
+  
+              <!-- Topbar -->
+              <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+          
+                  <!-- Sidebar Toggle (Topbar) -->
+                    <!-- Sidebar Toggle (Topbar) -->
+                  <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                    <i class="fa fa-bars"></i>
+                  </button>
+                  <button onclick="goBack()" class=" btn btn-warning text-white btn-block btn-sm">Go Back</button>
+                  <!-- Topbar Navbar -->
+                  <ul class="navbar-nav ml-auto">
+          
+                      <div class="topbar-divider d-none d-sm-block"></div>
+          
+                      <!-- Nav Item - User Information -->
+                      <li class="nav-item dropdown no-arrow">
+                      <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      {{-- <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{Auth::user()->name}}</span> --}}
+                          <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
+                      </a>
+                      <!-- Dropdown - User Information -->
+                      <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                          
+                          <div class="dropdown-divider"></div>
+                          <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                          <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                          Logout
+                          </a>
+                      </div>
+                      </li>
+          
+                  </ul>
+          
+              </nav>
+              <main>
+                  @yield('content')
+              </main>
+          </div>
+          <!-- End of Main Content -->
+      
+          <!-- Footer -->
+          <footer class="sticky-footer bg-white">
+          <div class="container my-auto">
+              <div class="copyright text-center my-auto">
+              <span>Copyright &copy; Your Website 2019</span>
+              </div>
+          </div>
+          </footer>
+          <!-- End of Footer -->
+
+      </div>
+      <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+  </div>
       
         <!-- Scroll to Top Button-->
         <a class="scroll-to-top rounded" href="#page-top">
@@ -236,7 +225,6 @@
             </div>
           </div>
         </div>
-    </div> 
     <!-- Bootstrap core JavaScript-->
     <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
     <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
