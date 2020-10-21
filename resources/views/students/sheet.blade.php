0bbaab8ed01b1d49890df1fe69bc04bcd6bce4d1
ikeogu <ikeogu322@gmail.com>
2020-10-21 13:00:12 +0100

    uN ASSIGNED SUBJECT LINK

--- resources/views/students/sheet.blade.php
+++ resources/views/students/sheet.blade.php
@@ -34,20 +34,20 @@
         @if ($class_T->status === 'High School')
         <div class="row">
             <div class="col-4 d-flex ">
-            <a href="{{route('cat1',[$student->id,$term->id,$class_T->id])}}" class="col-4 btn btn-block btn-success">C.A.T 1</a>
+            <a href="{{route('cat1',[$student->id,$term->id,$class_T->id])}}" class="col-4 col-sm-4 btn btn-block btn-success">C.A.T 1</a>
             </div>
             <div class="col-4 d-flex justify-content-between">
-            <a href="{{route('cat2',[$student->id,$term->id,$class_T->id])}}" class="col-4 btn btn-block btn-success">C.A.T 2</a>
+            <a href="{{route('cat2',[$student->id,$term->id,$class_T->id])}}" class="col-4 btn  col-sm-4 btn-block btn-success">C.A.T 2</a>
             </div>
             <div class="col-4 d-flex justify-content-end">
-            <a href="{{route('result',[$student->id,$term->id,$class_T->id])}}" class="col-4 btn btn-block btn-success ">Result </a>
+            <a href="{{route('result',[$student->id,$term->id,$class_T->id])}}" class="col-4 btn col-sm-4 btn-block btn-success ">Result </a>
             </div>
           </div> 
         @else
         <div class="row">
-            <a href="{{route('sum',[$student->id,$term->id,$class_T->id])}}" class="col-4 btn btn-block btn-success">Summative </a>
+            <a href="{{route('sum',[$student->id,$term->id,$class_T->id])}}" class="col-4 col-sm-6 btn btn-block btn-success">Summative </a>
             <div class="col-4 d-flex justify-content-end">
-            <a href="{{route('result',[$student->id,$term->id,$class_T->id])}}" class="col-4 btn btn-block btn-success ">Result </a>
+            <a href="{{route('result',[$student->id,$term->id,$class_T->id])}}" class="col-4  col-sm-6 btn btn-block btn-success ">Result </a>
             </div>
           </div> 
         @endif
