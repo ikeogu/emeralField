a26f82ecffaa7cce4b61f3361ce5ffe6535e1903
ikeogu <ikeogu322@gmail.com>
2020-10-27 19:25:31 +0100

    fixing style pdf

--- config/dompdf.php
+++ config/dompdf.php
@@ -38,7 +38,7 @@ return array(
          * Times-Roman, Times-Bold, Times-BoldItalic, Times-Italic,
          * Symbol, ZapfDingbats.
          */
-        "font_dir" => public_path('storage/fonts/'), // advised by dompdf (https://github.com/dompdf/dompdf/pull/782)
+        "font_dir" => public_path('storage/fonts'), // advised by dompdf (https://github.com/dompdf/dompdf/pull/782)
 
         /**
          * The location of the DOMPDF font cache directory
@@ -48,7 +48,7 @@ return array(
          *
          * Note: This directory must exist and be writable by the webserver process.
          */
-        "font_cache" => public_path('storage/fonts/'),
+        "font_cache" => public_path('storage/fonts'),
 
         /**
          * The location of a temporary directory.
