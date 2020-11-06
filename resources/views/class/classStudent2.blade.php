4b99338190ce630653bf2f67034cdfb4a7d76418
ikeogu <ikeogu322@gmail.com>
2020-11-06 08:49:24 +0100

    fixing UI

--- resources/views/class/classStudent2.blade.php
+++ resources/views/class/classStudent2.blade.php
@@ -21,7 +21,7 @@
             </div>
             
         </div>
-        @if ($class_T->status === 'Junior High School' || 'Senior High School')
+        @if ($class_T->status === 'Junior High School')
         <div class="row mt-5">
             <div class="col-md-2 mb-2 ">
                 <a href="{{route('cat1s_ct',[$t->id,$class_T->id])}}"  class="btn btn-success">C.A.T 1</a>
@@ -42,7 +42,46 @@
                 <a href="{{route('gt_ct',[$t->id,$class_T->id])}}"  class="btn btn-success">Grand Total</a>
             </div>
         </div>
-        @else
+        @endif
+        @if($class_T->status ==='Senior High School')
+        <div class="row mt-5">
+            <div class="col-md-2 mb-2 ">
+                <a href="{{route('cat1s_ct',[$t->id,$class_T->id])}}"  class="btn btn-success">C.A.T 1</a>
+            </div>
+            <div class="col-md-2 mb-2">
+                <a href="{{route('cat2s_ct',[$t->id,$class_T->id])}}"  class="btn btn-success">C.A.T 2</a>
+            </div>
+            <div class="col-md-2 mb-2">
+                <a href="{{route('msc_ct',[$t->id,$class_T->id])}}"  class="btn btn-success">MSC</a>
+            </div>
+            <div class="col-md-2 mb-2">
+                <a href="{{route('tca_ct',[$t->id,$class_T->id])}}"  class="btn btn-success">T.C.A</a>
+            </div>
+            <div class=" col-md-2 mb-2">
+                <a href="{{route('exam_ct',[$t->id,$class_T->id])}}"  class="btn btn-success">Exam </a>
+            </div>
+            <div class="col-md-2">
+                <a href="{{route('gt_ct',[$t->id,$class_T->id])}}"  class="btn btn-success">Grand Total</a>
+            </div>
+        </div>
+        @endif
+        @if($class_T->status ==='Year School')
+        <div class="row mt-5">
+            <div class="col-md-3">
+                <a href="{{route('summative_ct',[$t->id,$class_T->id])}}"  class="btn btn-success">Summative </a>
+            </div>
+            <div class="col-md-3">
+                <a href="{{route('tca_ct',[$t->id,$class_T->id])}}"  class="btn btn-success">TCA BroadSheet</a>
+            </div>
+            <div class=" col-md-3 py-3">
+                <a href="{{route('exam_ct',[$t->id,$class_T->id])}}"  class="btn btn-success">Exam BroadSheet</a>
+            </div>
+            <div class="col-md-3">
+                <a href="{{route('gt_ct',[$t->id,$class_T->id])}}"  class="btn btn-success">Grand Total BroadSheet</a>
+            </div>
+        </div>
+        @endif
+        @if($class_T->status ==='Early Years')
         <div class="row mt-5">
             <div class="col-md-3">
                 <a href="{{route('summative_ct',[$t->id,$class_T->id])}}"  class="btn btn-success">Summative </a>
--- resources/views/class/studentClass.blade.php
+++ resources/views/class/studentClass.blade.php
@@ -64,7 +64,24 @@
                 <a href="{{route('gt',[$t->id,$class_T->id])}}"  class="btn btn-success">Grand Total</a>
             </div>
         </div>
-        @else
+        @endif
+        @if($class_T->status === 'Year School')
+        <div class="row mt-5">
+            <div class="col-md-3">
+                <a href="{{route('summative',[$t->id,$class_T->id])}}"  class="btn btn-success">Summative </a>
+            </div>
+            <div class="col-md-3">
+                <a href="{{route('tca',[$t->id,$class_T->id])}}"  class="btn btn-success">TCA BroadSheet</a>
+            </div>
+            <div class=" col-md-3 py-3">
+                <a href="{{route('exam',[$t->id,$class_T->id])}}"  class="btn btn-success">Exam BroadSheet</a>
+            </div>
+            <div class="col-md-3">
+                <a href="{{route('gt',[$t->id,$class_T->id])}}"  class="btn btn-success">Grand Total BroadSheet</a>
+            </div>
+        </div>
+        @endif
+        @if($class_T->status === 'Early Years')
         <div class="row mt-5">
             <div class="col-md-3">
                 <a href="{{route('summative',[$t->id,$class_T->id])}}"  class="btn btn-success">Summative </a>
