6c02a12bea35a44bcead7f42e494749c80180b0c
ikeogu <ikeogu322@gmail.com>
2020-11-06 08:42:06 +0100

    fixing UI

--- resources/views/class/studentClass.blade.php
+++ resources/views/class/studentClass.blade.php
@@ -21,7 +21,29 @@
             </div>
             
         </div>
-        @if ($class_T->status == 'Junior High School' || 'Senior High School')
+        @if ($class_T->status === 'Senior High School')
+        <div class="row mt-5">
+            <div class="col-md-2 mb-2 ">
+                <a href="{{route('cat1s',[$t->id,$class_T->id])}}"  class="btn btn-success">C.A.T 1</a>
+            </div>
+            <div class="col-md-2 mb-2">
+                <a href="{{route('cat2s',[$t->id,$class_T->id])}}"  class="btn btn-success">C.A.T 2</a>
+            </div>
+            <div class="col-md-2 mb-2">
+                <a href="{{route('msc',[$t->id,$class_T->id])}}"  class="btn btn-success">MSC</a>
+            </div>
+            <div class="col-md-2 mb-2">
+                <a href="{{route('tca',[$t->id,$class_T->id])}}"  class="btn btn-success">T.C.A</a>
+            </div>
+            <div class=" col-md-2 mb-2">
+                <a href="{{route('exam',[$t->id,$class_T->id])}}"  class="btn btn-success">Exam </a>
+            </div>
+            <div class="col-md-2">
+                <a href="{{route('gt',[$t->id,$class_T->id])}}"  class="btn btn-success">Grand Total</a>
+            </div>
+        </div>
+        @endif
+        @if ($class_T->status === 'Junior High School')
         <div class="row mt-5">
             <div class="col-md-2 mb-2 ">
                 <a href="{{route('cat1s',[$t->id,$class_T->id])}}"  class="btn btn-success">C.A.T 1</a>
