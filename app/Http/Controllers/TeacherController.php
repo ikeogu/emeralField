87521ebf65a1bbfbd29df59b18433ec1cb6b3956
ikeogu <ikeogu322@gmail.com>
2020-11-07 01:10:46 +0100

    hello

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -39,7 +39,6 @@ class TeacherController extends Controller
     public function classt($id){
         $te = Teacher::find($id);
         $ct = ClassTeacher::where('teacher_id',$te->id)->get();
-        dd(ClassTeacherResource::collection($ct));
         return view('teacher.classteacher',['classt'=> ClassTeacherResource::collection($ct)]);
     }
 }
