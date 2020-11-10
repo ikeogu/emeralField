306fcc803c34f6380af430a9e7b62890553c97bf
ikeogu <ikeogu322@gmail.com>
2020-11-10 14:03:58 +0100

    message

--- app/Http/Controllers/Api/TeacherController.php
+++ app/Http/Controllers/Api/TeacherController.php
@@ -26,7 +26,7 @@ class TeacherController extends Controller
     public function index()
     {
         //
-        return TeacherResource::collection(Teacher::paginate(40));
+        return TeacherResource::collection(Teacher::all());
     }
 
     /**
